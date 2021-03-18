f62dae442e93b9813a514f5824ed5f108b0eec21
Hayen Phan <hayenphan@outlook.com>
2021-03-18 15:04:13 +0100

    Add homepage.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+// import Button from "../components/Button/Button";
+import style from './Home.module.scss';
+
+class Home extends React.Component {
+  render() {
+    return (
+      // {/* Place sections BETWEEN Container
+      //   <HomeVideo />
+      //   <Footer />
+      // */}
+
+      <Container>
+        <div>
+          Homepage
+        </div>
+      </Container>
+    )
+  }
+}
+
+export default Home;
