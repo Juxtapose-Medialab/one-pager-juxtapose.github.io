8533516d399598a89079c3e7d6fdd6ef75a997b6
Hayen Phan <hayenphan@outlook.com>
2021-03-22 09:29:05 +0100

    Add content.

--- src/components/Productintrosection/ProductIntroSection.jsx
+++ src/components/Productintrosection/ProductIntroSection.jsx
@@ -1,27 +1,46 @@
 import React from "react";
 import { Container, Row, Col } from 'react-bootstrap';
-import style from './ProductIntroSection.module.scss';
-
-import ProductImage from '../ProductImage/ProductImage'
+import styles from './ProductIntroSection.module.scss';
+import scheveningenImage1 from '../../static/images/druktescheveningen.jpeg';
+import mockup from '../../static/images/mockup.png';
+import mockbeach from '../../static/images/mockonbeach.png';
 
 class ProductIntroSection extends React.Component {
   render() {
     return (
-        <Container>
+      <>
+        <h2 className={ styles.productTitle }>Product</h2>
         <Row>
-          <Col>
-            <p className={ style.text }>Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt
-            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-            in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <Col className={ styles.col } xs={ 6 }>
+            <p className={ styles.productText }>Het concept bestaat uit digitale borden, met een camera/sensor, die verspreid staan door Scheveningen en Den Haag. Op deze borden is te zien hoe druk het is op het strand, zodat mensen de drukke plekken vermijden en opzoek kunnen gaan naar de minder drukke plekken. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam velit et mi faucibus blandit. Cras bibendum, justo elementum pellentesque posuere, quam purus molestie urna, accumsan dapibus ex enim at nisi. Maecenas at tincidunt est. Mauris dignissim ex nunc, rhoncus blandit massa tempor ut. Etiam suscipit turpis quis ante gravida egestas.  </p>
           </Col>
-          <Col>
-            <ProductImage />
+          <Col xs={ 6 }>
+            <div className={ styles.productImageWrapper }>
+              <img src={ scheveningenImage1 } className={ styles.productImage } />
+            </div>
           </Col>
         </Row>
-      </Container>
+        <Row>
+          <Col className={ styles.col } xs={ 6 }>
+            <div className={ styles.productImageWrapper2 }>
+              <img src={ mockup } className={ styles.productImage2 } />
+            </div>
+          </Col>
+          <Col className={ styles.col2 } xs={ 6 }>
+            <p className={ styles.productText2 }>Het concept bestaat uit digitale borden, met een camera/sensor, die verspreid staan door Scheveningen en Den Haag. Op deze borden is te zien hoe druk het is op het strand, zodat mensen de drukke plekken vermijden en opzoek kunnen gaan naar de minder drukke plekken. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam velit et mi faucibus blandit. Cras bibendum, justo elementum pellentesque posuere, quam purus molestie urna, accumsan dapibus ex enim at nisi. Maecenas at tincidunt est. Mauris dignissim ex nunc, rhoncus blandit massa tempor ut. Etiam suscipit turpis quis ante gravida egestas.  </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className={ styles.col } xs={ 6 }>
+            <p className={ styles.productText }>Het concept bestaat uit digitale borden, met een camera/sensor, die verspreid staan door Scheveningen en Den Haag. Op deze borden is te zien hoe druk het is op het strand, zodat mensen de drukke plekken vermijden en opzoek kunnen gaan naar de minder drukke plekken. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam velit et mi faucibus blandit. Cras bibendum, justo elementum pellentesque posuere, quam purus molestie urna, accumsan dapibus ex enim at nisi. Maecenas at tincidunt est. Mauris dignissim ex nunc, rhoncus blandit massa tempor ut. Etiam suscipit turpis quis ante gravida egestas.  </p>
+          </Col>
+          <Col xs={ 6 }>
+            <div className={ styles.productImageWrapper }>
+              <img src={ mockbeach } className={ styles.productImage } />
+            </div>
+          </Col>
+        </Row>
+        </>
     )
   }
 }
