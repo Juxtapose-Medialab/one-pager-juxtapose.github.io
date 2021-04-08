8bc9a0af6a6031378d3958fac7085a5f1667d022
Hayen Phan <hayenphan@outlook.com>
2021-04-08 23:50:29 +0200

    chore(app): Change TeamMembers text.

--- src/components/TeamMembers/TeamMembers.jsx
+++ src/components/TeamMembers/TeamMembers.jsx
@@ -30,7 +30,9 @@ class TeamMembers extends React.Component {
                     <p className={ styles.teamMemberTitle }>Designer</p>
                   </div>
                   <h2 className={ styles.teamMemberName }>Julia Van der Meijden</h2>
-                  <p className={ styles.teamMemberText }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed augue ut velit vehicula malesuada non at diam. Fusce ac tristique libero. Duis gravida augue vitae tempor pharetra. Quisque porta, metus vel fermentum pharetra, odio magna ultricies est, auctor pellentesque ex massa in felis.  Integer bibendum tincidunt mauris, vehicula commodo libero aliquet pretium. Curabitur ullamcorper nisi nibh. </p>
+                  <p className={ styles.teamMemberText }>Mijn naam is Julia, 24 jaar en ik ben een 3e jaars CMD'er. Naast dit project ben ik ook peercoach binnen mijn opleiding.
+
+Ik ben iemand die niet om de zaken heen draait en altijd eerlijk feedback probeert te geven. Dit kan soms voor wat gezond "head-butting" zorgen, maar ik ben van mening dat dit (in combinatie met een relaxte en open sfeer binnen het team) resulteert in een beter eind resultaat.</p>
                 </div>
               </div>
               <div className={ styles.teamMemberCard }>
@@ -42,7 +44,7 @@ class TeamMembers extends React.Component {
                     <p className={ styles.teamMemberTitle }>Designer</p>
                   </div>
                   <h2 className={ styles.teamMemberName }>Frank Hendriks</h2>
-                  <p className={ styles.teamMemberText }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed augue ut velit vehicula malesuada non at diam. Fusce ac tristique libero. Duis gravida augue vitae tempor pharetra. Quisque porta, metus vel fermentum pharetra, odio magna ultricies est, auctor pellentesque ex massa in felis.  Integer bibendum tincidunt mauris, vehicula commodo libero aliquet pretium. Curabitur ullamcorper nisi nibh. </p>
+                  <p className={ styles.teamMemberText }>Ik ben Frank Hendriks. Derdejaars Communication and Multimedia Design student. Naast mijn opleiding ben ik streamer op Twitch en voetbal ik in het eerste elftal bij mijn plaatselijke voetbalvereniging. Ik geef altijd eerlijke feedback en probeer met humor de sfeer in de groep te bevorderen.</p>
                 </div>
               </div>
             </Col>
@@ -69,7 +71,7 @@ class TeamMembers extends React.Component {
                     <p className={ styles.teamMemberTitle }>Developer</p>
                   </div>
                   <h2 className={ styles.teamMemberName }>Hayen Phan</h2>
-                  <p className={ styles.teamMemberText }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed augue ut velit vehicula malesuada non at diam. Fusce ac tristique libero. Duis gravida augue vitae tempor pharetra. Quisque porta, metus vel fermentum pharetra, odio magna ultricies est, auctor pellentesque ex massa in felis.  Integer bibendum tincidunt mauris, vehicula commodo libero aliquet pretium. Curabitur ullamcorper nisi nibh. </p>
+                  <p className={ styles.teamMemberText }>Ik ben Hayen, 22 jaar en volg op het moment de opleiding CMGT. Overdag ben ik vaak bezig met programmeren, maar in de avond besteed ik mijn tijd aan het bespelen en maken van muziek. Ik zie mijzelf als een perfectionistisch, nogal chaotisch, maar wel ambitieuze persoon die er altijd voor openstaat om mensen gerust te stellen en te helpen.</p>
                 </div>
               </div>
               <div className={ styles.teamMemberCard }>
@@ -81,7 +83,7 @@ class TeamMembers extends React.Component {
                     <p className={ styles.teamMemberTitle }>Developer</p>
                   </div>
                   <h2 className={ styles.teamMemberName }>Carli Nabbe</h2>
-                  <p className={ styles.teamMemberText }>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sed augue ut velit vehicula malesuada non at diam. Fusce ac tristique libero. Duis gravida augue vitae tempor pharetra. Quisque porta, metus vel fermentum pharetra, odio magna ultricies est, auctor pellentesque ex massa in felis.  Integer bibendum tincidunt mauris, vehicula commodo libero aliquet pretium. Curabitur ullamcorper nisi nibh. </p>
+                  <p className={ styles.teamMemberText }>Ik ben Carli. 28 jaar en van de opleiding CMGT. In mijn vrije tijd ben ik voornamelijk bezig met verschillende games, coderen of een goede serie te kijken. Ondertussen helemaal onder geplakt met plakplaatjes maar voor de rest niet gevaarlijk. Ik zie mijzelf als een rustig persoon die goed kan luisteren en mensen van logische feedback voorziet.</p>
                 </div>
               </div>
             </Col>
